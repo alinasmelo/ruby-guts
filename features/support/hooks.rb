05af5ddccba6348fa66a959e65656a8a62bc1369
AlinaMelo <alina.melo@cwi.com.br>
2020-09-09 23:25:02 -0300

    firt commit

---
+++ features/support/hooks.rb
@@ -0,0 +1,10 @@
+Before do
+    @app = App.new
+end
+
+After do |scenario|
+    if scenario.failed?
+        page.save_screenshot("screenshots/#{scenario.name}#{time.now}_screenshot.pnj")
+
+    end
+end
